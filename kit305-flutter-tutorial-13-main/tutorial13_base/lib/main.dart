e21bee58204b7320ebd3fa37c70e518fe81b01a3
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-03 20:26:46 +1000

    Update main.dart

--- kit305-flutter-tutorial-13-main/tutorial13_base/lib/main.dart
+++ kit305-flutter-tutorial-13-main/tutorial13_base/lib/main.dart
@@ -147,70 +147,7 @@ class FullScreenText extends StatelessWidget {
   }
 }
 
-/*
-import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
-      ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  List<int> items = List<int>.generate(100, (int index) => index);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      itemBuilder: (BuildContext context, int index) {
-        return Dismissible(
-          background: Container(
-            color: Colors.green,
-          ),
-          key: ValueKey<int>(items[index]),
-          onDismissed: (DismissDirection direction) {
-            setState(() {
-              items.removeAt(index);
-            });
-          },
-          child: ListTile(
-            title: Text(
-              'Item ${items[index]}',
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-*/
-
-/*
-//Only copy this code in temporarily if you want to learn more about FutureBuilder. Undo its addition  once done.
-class MyApp extends StatelessWidget
-{
   // Create the initialization Future outside of `build`:
   final Future<String> Function() test = () async
   {
@@ -236,4 +173,3 @@ class MyApp extends StatelessWidget
     );
   }
 }
-*/
